c8318e4747d29c608772d1e310a19d683d0b22f6
Grace Zeng <gracezeng14@gmail.com>
2024-09-13 09:40:14 -0400

    number 100 reduced

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        for(int i = 100; i > 0;){
+            if(i%2 == 0) i = i/2;
+            else i = i-1;
+            counter += 1;
+        }
+        System.out.println(counter);
+
+    }
+
 }
